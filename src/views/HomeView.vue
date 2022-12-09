a491b929daa3173334dfd15fd7303c9284f115e7
Adhi Wiratomo <adhi.wiratomo@uii.ac.id>
2022-12-09 18:07:34 +0700

    feat: add feature update delete create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 
     <v-row>
       <v-col class="ml-auto" lg="3" md="4" sm="12">
-        <v-btn @click="dialog.form = true" color="primary" block class="text-capitalize">
+        <v-btn @click="dialog.form = true; type_detail = 'CREATE'" color="primary" block class="text-capitalize">
           Tambah
         </v-btn>
       </v-col>
@@ -15,13 +15,13 @@
           :items="employees"
           class="elevation-1">
           <!-- eslint-disable-next-line vue/valid-v-slot -->
-          <template #item.actions>
+          <template #item.actions="{item, index}">
             <td class="text-center">
-              <v-btn icon>
+              <v-btn @click="updateEmployee(item, index)" icon>
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
 
-              <v-btn icon>
+              <v-btn @click="deleteEmployee(item, index)" icon>
                 <v-icon>mdi-delete</v-icon>
               </v-btn>
             </td>
@@ -45,7 +45,7 @@
               <v-text-field v-model="formEmployee.phone" label="Nomor handphone"></v-text-field>
               <v-text-field v-model="formEmployee.email" label="Email"></v-text-field>
               
-              <v-autocomplete label="Posisi" :items="positions" item-text="name" item-value="_id"></v-autocomplete>
+              <v-autocomplete v-model="formEmployee.position_id" label="Posisi" :items="positions" item-text="name" item-value="_id"></v-autocomplete>
             </v-form>
           </div>
         </v-card-text>
@@ -65,8 +65,42 @@
           <v-btn
             color="primary"
             text
-            @click="addEmployee()">
-            Simpan
+            @click="submit()">
+            {{ type_detail === 'CREATE' ? 'Simpan' : 'Update' }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialog.delete" width="520">
+      <v-card>
+        <v-card-title class="text-h5 error lighten-2">
+          Hapus Karyawan
+        </v-card-title>
+
+        <v-card-text class="d-flex align-center mb-0">
+          <div class="pa-2 text-center">
+            <span class="text-body-2">Apakah anda yakin ingin menghapus pegawai <b class="error--text">{{ detail.name }}</b> ??</span>
+          </div>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="error"
+            text
+            @click="dialog.delete = false"
+          >
+            Batal
+          </v-btn>
+
+          <v-btn
+            color="primary"
+            text
+            @click="deleted()">
+            Hapus
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -79,7 +113,7 @@
   export default {
     data () {
       return {
-        dialog: { form: false },
+        dialog: { form: false, delete: false },
         employees: [],
         positions: [],
         headers: [
@@ -101,8 +135,10 @@
           address: '',
           phone: '',
           email: '',
-          position: ''
-        }
+          position_id: ''
+        },
+        type_detail: '',
+        detail: {}
       }
     },
     mounted() {
@@ -136,15 +172,64 @@
         })
       },
 
-      addEmployee() {
+      submit() {
         const configHeader = {
           'Authorization': `Bearer ${this.getUser.token}`,
           'Content-Type': 'application/json'
         }
-        this.axios.post('http://13.214.194.163:5400/api/v1/positions/create', this.formEmployee, { headers: configHeader }).then((response) => {
-          console.log(response);
-        })
+        if(this.type_detail === 'CREATE') {
+          this.axios.post('http://13.214.194.163:5400/api/v1/employees/create', this.formEmployee, { headers: configHeader })
+          .then((response) => {
+            console.log(response);
+            this.dialog.form = false;
+            this.getData();
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        }
+
+        if(this.type_detail === 'UPDATE') {
+          this.axios.put(`http://13.214.194.163:5400/api/v1/employees/update/${this.detail._id}`, this.formEmployee, { headers: configHeader })
+          .then((response) => {
+            console.log(response);
+            this.dialog.form = false;
+            this.getData();
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        }
+      },
+
+      updateEmployee(employee, index) {
+        this.type_detail = 'UPDATE';
+        if(employee) 
+          this.dialog.form = true;
+          this.formEmployee = {...employee};
+          this.detail = this.employees[index];
+      },
+
+      deleteEmployee(employee, index) {
+        if(employee) 
+          this.dialog.delete = true;
+          this.detail = this.employees[index];
+      },
+
+      deleted() {
+        const configHeader = {
+          'Authorization': `Bearer ${this.getUser.token}`
+        }
+        this.axios.delete(`http://13.214.194.163:5400/api/v1/employees/delete/${this.detail._id}`, { headers: configHeader })
+          .then((response) => {
+            console.log(response);
+            this.dialog.delete = false;
+            this.getData();
+          })
+          .catch(error => {
+            console.log(error)
+          })
       }
-    },
+    }
   }
 </script>
